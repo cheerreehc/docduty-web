0baeccc6308863d77aa5562c8be654fdf2c3c70f
Oborm <oborm@CHs-MacBook-Pro.local>
2025-07-08 15:40:13 +0700

    fix plesk

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true, // ✅ ปิด eslint ตอน build
   },
   output: 'export', // ✅ เพิ่มตรงนี้เพื่อใช้ static export
+  trailingSlash: true,
 };
 
 export default nextConfig;
