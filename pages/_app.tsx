72d9b0e20e797421b6ebd8b2cd6b14ec3af9cf48
Oborm <oborm@CHs-MacBook-Pro.local>
2025-07-18 00:27:48 +0700

    feat: logo loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,47 @@ import { DutyTypeProvider } from '@/contexts/DutyTypeContext'
 import { WorkspaceProvider } from '@/contexts/WorkspaceContext'
 import { ScheduleProvider } from '@/contexts/ScheduleContext'; 
 import { MemberProvider } from '@/contexts/MemberContext';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [isPageLoading, setIsPageLoading] = useState(false);
+
+   useEffect(() => {
+    const handleStart = () => setIsPageLoading(true);
+    const handleComplete = () => setIsPageLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
   return (
     <UserProvider>
       <WorkspaceProvider>
         <MemberProvider>
         <DutyTypeProvider>
           <ScheduleProvider> 
+            {isPageLoading && (
+              <div className="fixed inset-0 bg-white/70 z-[9999] flex items-center justify-center">
+                <Image
+                  src="/logo-docduty.png"
+                  alt="Loading"
+                  width={120}
+                  height={120}
+                  className="animate-bounce"
+                />
+              </div>
+            )}
             <Component {...pageProps} />
             <Toaster richColors position="top-center" theme="light" />
           </ScheduleProvider>
