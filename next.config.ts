8dce60b7fd7081499f762d41e13f6103983230c6
Oborm <oborm@CHs-MacBook-Pro.local>
2025-07-18 18:52:33 +0700

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,13 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  // ✅ เพิ่ม matcher เพื่อให้ middleware ทำงานกับทุกหน้า
-  matcher: ['/((?!_next|favicon.ico|images|fonts|public).*)'],
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  ...(isProd && {
-    output: 'export',
-  }),
   images: {
-    domains: ['pqnmfikncumnshlbgsmz.supabase.co'], // ✅ ใส่โดเมนของ supabase ที่ใช้ storage
+    // ใส่โดเมนของ supabase ที่ใช้ storage
+    domains: ['pqnmfikncumnshlbgsmz.supabase.co'], 
   },
 };
 
